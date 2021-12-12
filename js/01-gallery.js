5a5b8ca58301eaf0c488be120b156dd7e5cd9079
RuslanVidnichuk <monnaliza139@gmail.com>
2021-12-12 17:50:44 +0200

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImgCardsMarkup(galleryItems) {
+   return  galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a> 
+        </div>
+        `
+    }).join("");
+};
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return
+    };
+
+    const originalImgUrl = event.target.dataset.source;
+    const createModalWindow = basicLightbox.create(`
+    <img src="${originalImgUrl}" width="800" height="600">`)
+
+    createModalWindow.show()
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgCardsMarkup(galleryItems);
+const optionsLightBox = {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+};
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImgCardsMarkup(galleryItems) {
+   return  galleryItems.map(({ preview, original, description }) => {
+       return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    }).join("");
+};
+
+let lightbox = new SimpleLightbox(".gallery a", optionsLightBox);
